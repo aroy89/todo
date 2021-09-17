711ef0f96cf354cfa1f42570cc492371e36b7cbb
unknown <abhijit.roy.career@gmail.com>
2021-09-18 00:44:17 +0530

    Initialize project using Create React App

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from './Form';
+import TodoList from './TodoList';
+
+const App = () =>{
+
+    return(
+        <>
+        <Form />
+        <TodoList />
+        </>
+    );
+}
+
+export default App;
---
+++ Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form = () => {
+
+    return (
+        <Form>
+            <input type="text" name="form" className="form"/>
+            <button>Add</button>
+        </Form>
+       
+    );
+}
+
+export default Form;
